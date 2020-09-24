3dd882763548fa0ffc4e30c938c884784d9b759b
faaat <zjf580@163.com>
2020-09-24 21:38:19 +0800

    添加debug日志

--- core/ApiReturn.php
+++ core/ApiReturn.php
@@ -20,14 +20,6 @@ class ApiReturn {
         $this->data = $data;
         $this->code = $code;
         $this->msg = $msg;
-        if (DEBUG_MODE) {
-            //add api log
-            $logFile = LOG_DIR . 'access/' . date('Ymd') . '/';
-            if (!is_dir($logFile)) {
-                mkdir($logFile, 0755, true);
-            }
-            file_put_contents($logFile . 'access_' . date('H') . '.log', date('Y-m-d H:i:s') . '|' . ($_SERVER['REQUEST_URI'] ?? '') . '|' . json_encode(json_decode(file_get_contents("php://input"), TRUE)) . '|' . json_encode(array('code' => $code, 'data' => $data, 'msg' => $msg), JSON_UNESCAPED_UNICODE) . PHP_EOL, FILE_APPEND);
-        }
     }
     
     public static function init($data, $code = 200, $msg = '')
--- public/index.php
+++ public/index.php
@@ -43,5 +43,14 @@ try {
     $return = array('status' => 'error', 'data' => '', 'msg' => $e->getMessage());
 }
 
+if (DEBUG_MODE) {
+    //add api log
+    $logFile = LOG_DIR . 'access/' . date('Ymd') . '/';
+    if (!is_dir($logFile)) {
+        @mkdir($logFile, 0755, true);
+    }
+    @file_put_contents($logFile . 'access_' . date('H') . '.log', date('Y-m-d H:i:s') . '|' . ($_SERVER['REQUEST_URI'] ?? '') . '|' . json_encode(json_decode(file_get_contents("php://input"), TRUE)) . '|' . json_encode($return, JSON_UNESCAPED_UNICODE) . PHP_EOL, FILE_APPEND);
+}
+
 //var_dump($return);
 echo json_encode($return);
