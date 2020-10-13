9449091059edc530b3fe1ff1ff7e564139928c15
faaat <zjf580@163.com>
2020-10-13 19:47:57 +0800

    修复db链接bug

--- public/crons/success_withdraw_quick.php
+++ public/crons/success_withdraw_quick.php
@@ -4,37 +4,28 @@
 //  一小时执行一次
 require_once __DIR__ . '/../init.inc.php';
 
-$db = new NewPdo('mysql:dbname=' . DB_DATABASE . ';host=' . DB_HOST . ';port=' . DB_PORT, DB_USERNAME, DB_PASSWORD);
-$db->exec("SET time_zone = '+8:00'");
-$db->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
+$db = Db::getDbInstance();
 
 $model = new Model();
 $wechatPay = new Wxpay();
 while (true) {
-    try {
-        $db->beginTransaction();
-        $sql = 'SELECT * FROM t_withdraw WHERE withdraw_status = "pending" AND withdraw_amount = 1 AND withdraw_method = "wechat" ORDER BY withdraw_id';
-        $withdrawList = $db->getAll($sql);
+    $sql = 'SELECT * FROM t_withdraw WHERE withdraw_status = "pending" AND withdraw_amount = 1 AND withdraw_method = "wechat" ORDER BY withdraw_id';
+    $withdrawList = $db->getAll($sql);
 
-        foreach ($withdrawList as $withdrawInfo) {
-            $sql = 'SELECT COUNT(withdraw_id) FROM t_withdraw WHERE user_id = ? AND withdraw_amount = 1 AND withdraw_status = "success"';
-            if ($db->getOne($sql, $withdrawInfo['user_id'])) { //to do failure reason from api return
-                $model->withdraw->updateStatus(array('withdraw_status' => 'failure', 'withdraw_remark' => '新用户专享', 'withdraw_id' => $withdrawInfo['withdraw_id']));
+    foreach ($withdrawList as $withdrawInfo) {
+        $sql = 'SELECT COUNT(withdraw_id) FROM t_withdraw WHERE user_id = ? AND withdraw_amount = 1 AND withdraw_status = "success"';
+        if ($db->getOne($sql, $withdrawInfo['user_id'])) { //to do failure reason from api return
+            $model->withdraw->updateStatus(array('withdraw_status' => 'failure', 'withdraw_remark' => '新用户专享', 'withdraw_id' => $withdrawInfo['withdraw_id']));
+        } else {
+            $returnStatus = $wechatPay->transfer($withdrawInfo['withdraw_amount'], $withdrawInfo['wechat_openid']);
+            if (TRUE === $returnStatus) {
+                $model->gold->updateGold(array('user_id' => $withdrawInfo['user_id'], 'gold' => $withdrawInfo['withdraw_gold'], 'source' => "withdraw", 'type' => "out", 'relation_id' => $withdrawInfo['withdraw_id']));
+                $model->withdraw->updateStatus(array('withdraw_status' => 'success', 'withdraw_id' => $withdrawInfo['withdraw_id']));
             } else {
-                $returnStatus = $wechatPay->transfer($withdrawInfo['withdraw_amount'], $withdrawInfo['wechat_openid']);
-                if (TRUE === $returnStatus) {
-                    $model->gold->updateGold(array('user_id' => $withdrawInfo['user_id'], 'gold' => $withdrawInfo['withdraw_gold'], 'source' => "withdraw", 'type' => "out", 'relation_id' => $withdrawInfo['withdraw_id']));
-                    $model->withdraw->updateStatus(array('withdraw_status' => 'success', 'withdraw_id' => $withdrawInfo['withdraw_id']));
-                } else {
-                    //to do failure reason from api return
-                    $model->withdraw->updateStatus(array('withdraw_status' => 'failure', 'withdraw_remark' => $returnStatus, 'withdraw_id' => $withdrawInfo['withdraw_id']));
-                }
+                //to do failure reason from api return
+                $model->withdraw->updateStatus(array('withdraw_status' => 'failure', 'withdraw_remark' => $returnStatus, 'withdraw_id' => $withdrawInfo['withdraw_id']));
             }
         }
-        $db->commit();
-    } catch (Exception $e) {
-        $db->rollBack();
-        @file_put_contents(LOG_DIR . 'withdraw_1.log', $e->getMessage() . PHP_EOL, FILE_APPEND);
     }
 
     sleep(3);
